39d48be8e7c3a6cb28e3ff0316ad1099f7c7de41
LuiscVillarrealL <luiscvillarreall@gmail.com>
2024-06-12 00:36:49 +0100

    deleteErrorsImports

--- Assets/Systems/Minigames/ClosetGame/GrabClothesMinigame.cs
+++ Assets/Systems/Minigames/ClosetGame/GrabClothesMinigame.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class GrabClothesMinigame : MinigameBase
 {
